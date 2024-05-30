556a4644c7a07211b27ed7da02bf422e8032fda2
arjo31 <arjo3103@gmail.com>
2024-05-30 21:47:43 +0530

    Array Problem Added

---
+++ Arrays/Array problems/count_triplets_that_can_form_2_arrays_of_equal_xor.cpp
@@ -0,0 +1,67 @@
+/*Given an array of integers arr.
+
+We want to select three indices i, j and k where (0 <= i < j <= k < arr.length).
+
+Let's define a and b as follows:
+
+a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+Note that ^ denotes the bitwise-xor operation.
+
+Return the number of triplets (i, j and k) Where a == b.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force : O(n^3)
+int countTriplets(vector<int> &arr)
+{
+    int n = arr.size();
+    int count = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            int a = 0;
+            for (int k = i; k < j; ++k)
+            {
+                a ^= arr[k];
+            }
+
+            int b = 0;
+            for (int k = j; k < n; ++k)
+            {
+                b ^= arr[k];
+                if (a == b)
+                {
+                    ++count;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+// Optimized : O(n^2)
+int countTriplets(vector<int> &arr)
+{
+    int n = arr.size();
+    int count = 0;
+
+    vector<int> prefXor(n + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        prefXor[i + 1] = prefXor[i] ^ arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (prefXor[i] == prefXor[j + 1])
+                count += (j - i);
+        }
+    }
+    return count;
+}
