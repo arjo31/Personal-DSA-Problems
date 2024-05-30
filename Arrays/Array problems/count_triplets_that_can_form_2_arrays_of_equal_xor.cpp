ea6726fca7ead371ab6947be94d3e83e7b9e17f6
arjo31 <arjo3103@gmail.com>
2024-05-30 21:57:04 +0530

    Array Problem Added

--- Arrays/Array problems/count_triplets_that_can_form_2_arrays_of_equal_xor.cpp
+++ Arrays/Array problems/count_triplets_that_can_form_2_arrays_of_equal_xor.cpp
@@ -65,3 +65,26 @@ int countTriplets(vector<int> &arr)
     }
     return count;
 }
+
+//Another Method
+int countTriplets(vector<int> &arr)
+{
+    int n = arr.size();
+    unordered_map<int, vector<int>>mpp;
+    mpp[0].push_back(-1);
+    int x = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        x^=arr[i];
+        if (mpp.find(x)!=mpp.end())
+        {
+            for (auto a : mpp[x])
+            {
+                ans+=(i - (a + 1));
+            }
+        }
+        mpp[x].push_back(i);
+    }
+    return ans;
+}
