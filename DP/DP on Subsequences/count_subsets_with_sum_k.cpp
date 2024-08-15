5acdced388f5bb82347baf629b4ecdf67d6ed940
arjo31 <arjo3103@gmail.com>
2024-08-16 00:49:12 +0530

    Updated DP on subsequences

--- DP/DP on Subsequences/count_subsets_with_sum_k.cpp
+++ DP/DP on Subsequences/count_subsets_with_sum_k.cpp
@@ -1,52 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//arr[i] does not include the number 0;
+// arr[i] does not include the number 0;
 
-//Memoization
-int solveMemoization(int n, int k, vector<int>&arr, vector<vector<int>>&dp)
+// Memoization
+int solveMemoization(int n, int k, vector<int> &arr, vector<vector<int>> &dp)
 {
-    if (k==0) return 1;
-    if (n < 0) return 0;
-    if (dp[n][k]!=-1) return dp[n][k];
-    int notTake = solveMemoization(n-1,k,arr,dp);
+    if (k == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+    if (dp[n][k] != -1)
+        return dp[n][k];
+    int notTake = solveMemoization(n - 1, k, arr, dp);
     int take = 0;
-    if (arr[n]<=k)
+    if (arr[n] <= k)
     {
-        take = solveMemoization(n-1,k-arr[n],arr,dp);
+        take = solveMemoization(n - 1, k - arr[n], arr, dp);
     }
     dp[n][k] = notTake + take;
     return dp[n][k];
 }
 
-int countSubsetsMemoization(int n, int k, vector<int>&arr)
+int countSubsetsMemoization(int n, int k, vector<int> &arr)
 {
-    vector<vector<int>>dp(n,vector<int>(k+1,-1));
-    return solveMemoization(n-1,k,arr,dp);
+    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+    return solveMemoization(n - 1, k, arr, dp);
 }
 
-//Tabulation
-int countSubsetsTabulation(int n, int k, vector<int>&arr)
+// Tabulation
+int countSubsetsTabulation(int n, int k, vector<int> &arr)
 {
-    vector<vector<int>>dp(n,vector<int>(k+1,0));
+    vector<vector<int>> dp(n, vector<int>(k + 1, 0));
     for (int i = 0; i < n; i++)
     {
         dp[i][0] = 1;
     }
-    if (arr[0]<=k)
+    if (arr[0] <= k)
     {
         dp[0][arr[0]] = 1;
     }
 
     for (int i = 1; i < n; i++)
     {
-        for(int target = 0; target<=k; target++)
+        for (int target = 0; target <= k; target++)
         {
             int take = 0;
-            int notTake = dp[i-1][target];
-            if (arr[i]<=target) take = dp[i-1][target - arr[i]];
+            int notTake = dp[i - 1][target];
+            if (arr[i] <= target)
+                take = dp[i - 1][target - arr[i]];
             dp[i][target] = notTake + take;
         }
     }
-    return dp[n-1][k];
+    return dp[n - 1][k];
+}
+
+// If arr contains 0
+int solveMemoization(int n, int k, vector<int> &arr, vector<vector<int>> &dp)
+{
+    if (n==0){
+        if (k==0 && arr[0]==0) return 2;
+        if (arr[0]==k || k==0) return 1;
+        return 0;
+    }
+    if (dp[n][k] != -1)
+        return dp[n][k];
+    int notTake = solveMemoization(n - 1, k, arr, dp);
+    int take = 0;
+    if (arr[n] <= k)
+    {
+        take = solveMemoization(n - 1, k - arr[n], arr, dp);
+    }
+    dp[n][k] = notTake + take;
+    return dp[n][k];
 }
--- DP/DP on Subsequences/partition_array_to_make_min_sum_diff.cpp
+++ DP/DP on Subsequences/partition_array_to_make_min_sum_diff.cpp
@@ -1,38 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minSubsetSumDifference(vector<int>& nums, int n)
+int minSubsetSumDifference(vector<int> &arr, int n)
 {
-    int sum = accumulate(nums.begin(),nums.end(),0);
-    vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
-    for (int i = 0; i < n ; i++)
+    int sum = accumulate(arr.begin(), arr.end(), 0);
+    vector<vector<int>> dp(n + 1, vector<int>(sum + 1));
+
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+    for (int i = n - 1; i >= 0; i--)
     {
-        dp[i][0] = true;
-    }    
-    if (nums[0]<=sum)
-    {
-        dp[0][nums[0]] = true;
-    }
-    for (int i = 1; i < n; i++)
-    {
-        for (int target = 0; target<=sum; target++)
+        for (int s = 0; s <= sum; s++)
         {
-            bool take = false;
-            bool notake = dp[i-1][target];
-            if (nums[i]<=target) take = dp[i-1][target - nums[i]];
-            dp[i][target] = take || notake;
+            int notPick = dp[i + 1][s];
+            int pick = 0;
+            if (arr[i] <= s)
+                pick = dp[i + 1][s - arr[i]];
+            dp[i][s] = pick || notPick;
         }
     }
-    int _min = 1e9;
-    int s1 = 0, s2 = sum;
-    for (int i = 0; i <= sum/2 ; i++)
+    vector<int> diff = dp[0];
+    int minDiff = INT_MAX;
+    for (int i = 0; i <= sum / 2; i++)
     {
-        if (dp[n-1][i])
-        {
-            s1 = i;
-            s2 = sum-i;
-            _min = min(_min,abs(s1-s2));
-        }
+        if (diff[i])
+            minDiff = min(minDiff, sum - 2 * i);
     }
-    return _min;
+    return minDiff;
 }
