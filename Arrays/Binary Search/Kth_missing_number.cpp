6f1f1345f9084fe360dd4ee3342bdcfc7263adea
arjo31 <arjo3103@gmail.com>
2024-09-23 12:06:54 +0530

    Update K-th_missing_number

--- Arrays/Binary Search/Kth_missing_number.cpp
+++ Arrays/Binary Search/Kth_missing_number.cpp
@@ -1,17 +1,17 @@
-//You are given a strictly increasing array ‘vec’ and a positive integer ‘k’. Find the ‘kth’ positive integer missing from ‘vec’.
+// You are given a strictly increasing array ‘vec’ and a positive integer ‘k’. Find the ‘kth’ positive integer missing from ‘vec’.
 
 #include <bits/stdc++.h>
 using namespace std;
 
 const int MOD = 1e9 + 7;
 
-int missingK(vector<int>&arr, int k)
+int missingK(vector<int> &arr, int k)
 {
     int low = 0;
     int high = arr.size() - 1;
-    while (low<=high)
+    while (low <= high)
     {
-        int mid = low + (high - low)/2;
+        int mid = low + (high - low) / 2;
         int missing = arr[mid] - (mid + 1);
         if (missing < k)
         {
@@ -22,5 +22,6 @@ int missingK(vector<int>&arr, int k)
             high = mid - 1;
         }
     }
-    return k + high + 1;
+    int more = k - (arr[high] - (high + 1));
+    return arr[high] + more;
 }
